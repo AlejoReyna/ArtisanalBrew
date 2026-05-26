6f942d3bfd49247cb9811f9ca91a99efcccc0a7c
Alexis Reyna <alexis.reynasz@uanl.edu.mxw>
2026-05-26 13:54:06 -0600

    pls work

---
+++ src/ThisCafeteria.Infrastructure/Persistence/Migrations/20260526205000_SyncPendingModelChanges.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThisCafeteria.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncPendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_ProductId",
+                table: "OrderItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_ProductId",
+                table: "OrderItems",
+                column: "ProductId");
+        }
+    }
+}
